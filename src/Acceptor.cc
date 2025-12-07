f758b1892ccb8c69379ae478be315e1946e75011
ULookup <CHBulookup@outlook.com>
2025-12-07 21:51:43 +0800

    Acceptor 一种特殊的Connection，它只负责新连接到来后分发连接给子Reactor

---
+++ src/Acceptor.cc
@@ -0,0 +1,33 @@
+#include "Acceptor.h"
+#include <spdlog/spdlog.h>
+
+namespace webserver::src
+{
+
+Acceptor::Acceptor(EventLoop *loop, uint16_t port) 
+    : _loop(loop), _socket(), _channel(loop, -1)
+    {
+        bool ret = _socket.CreateServer(port);
+        assert(ret == true);
+        SPDLOG_TRACE("创建监听套接字");
+        _channel.SetFd(_socket.Fd());
+        _channel.SetReadCallback(std::bind(&Acceptor::HandleRead, this));
+        SPDLOG_TRACE("channel 设置读事件回调成功");
+    }
+
+void Acceptor::HandleRead() {
+    net::InetAddress peer;  // 创建一个网络地址对象，用于存储对端地址
+    std::shared_ptr<net::Socket> socket = _socket.Accept(peer); // 接收连接
+    if(!socket) {
+        // accept socket failed
+        SPDLOG_ERROR("Accept socket failed!");
+        return;
+    }
+    int newfd = socket->Fd();
+    if(newfd < 0) {
+        return;
+    }
+    if(_accept_callback) _accept_callback(newfd);
+}
+
+}
